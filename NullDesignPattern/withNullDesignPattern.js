f9474b8e65abe0ef7fbdc6df1e71af168d7b225b
seonjakim <seonja.kim0@gmail.com>
2020-08-08 16:06:31 +0900

    null design pattern

---
+++ NullDesignPattern/withNullDesignPattern.js
@@ -0,0 +1,46 @@
+//참조자료 : https://www.youtube.com/watch?v=D4Dja5WSZoA
+
+class User {
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  hasAccess() {
+    return this.name === "Bob";
+  }
+}
+
+class NullUser {
+  constructor() {
+    this.id = -1;
+    this.name = "Guest";
+  }
+
+  hasAccess() {
+    return false;
+  }
+}
+
+const users = [new User(1, "Bob"), new User(2, "John")];
+
+function getUser(id) {
+  const user = users.find((user) => user.id === id);
+  if (user == null) {
+    return new NullUser();
+  } else {
+    return user;
+  }
+}
+
+function printUser(id) {
+  const user = getUser(id);
+
+  console.log("Hello" + user.name);
+
+  if (user.hasAcess()) {
+    console.log("You have access");
+  } else {
+    console.log("You are not allowed here");
+  }
+}
---
+++ NullDesignPattern/withoutnullDesignPattern.js
@@ -0,0 +1,30 @@
+class User {
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  hasAccess() {
+    return this.name === "Bob";
+  }
+}
+
+const users = [new User(1, "Bob"), new User(2, "John")];
+
+function getUser(id) {
+  return users.find((user) => user.id === id);
+}
+
+function printUser(id) {
+  const user = getUser(id);
+
+  let name = "Guest";
+  if (user != null && user.name != null) name = user.name;
+  console.log("Hello" + name);
+
+  if (user != null && user.hasAccess != null && user.hasAcess()) {
+    console.log("You have access");
+  } else {
+    console.log("You are not allowed here");
+  }
+}
